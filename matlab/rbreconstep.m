0b8cd90dd4cc6c94b8f4248e2976c8cc5908a938
Qianqian Fang <fangqq@gmail.com>
2020-04-23 23:16:55 -0400

    handle multiple wavelength in rbreconstep

--- matlab/rbreconstep.m
+++ matlab/rbreconstep.m
@@ -31,7 +31,15 @@ function [dmu, misfit]=rbreconstep(cfg,sd,recon,detphi0,f2rid,f2rweight, reform)
 %Jmua=rbfemmatrix(cfg, sd, phi);    % use mex to build Jacobian, 2x faster
 Jmua=rbjacmuafast(sd, phi, cfg.nvol); % use approximated nodal-adjoint for mua
 %Jmua=rbjac(sd, phi, cfg.deldotdel, cfg.elem, cfg.evol); % or use native code to build nodal-based Jacobian for mua
-[Jmua, misfit]=rbcreateinv(Jmua, detphi0, detphi, cfg.params, reform);
+
+if(isa(Jmua,'containers.Map'))
+    [Jmua,detphi0,detphi]=rbinvmatrix(Jmua, detphi0, detphi);
+end
+if(nargin>=7)
+    [Jmua,misfit]=rbmatreform(Jmua, detphi0(:), detphi(:),reform);
+else
+    misfit=detphi(:)-detphi0(:)
+end
 Jmua_recon=meshremap(Jmua.',f2rid, f2rweight,recon.elem,size(recon.node,1)).'; 
 dmu_recon=rbreginv(Jmua_recon, misfit, 0.05);  % solve the update on the recon mesh
 dmu=meshinterp(dmu_recon,f2rid, f2rweight,recon.elem); % interpolate the update to the forward mesh
